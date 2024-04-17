66e1717b6d5a7e852354b4540ba3d84593a9ce2b
nikolayvf <nikolayvf@gmail.com>
2024-04-17 16:17:22 +0300

    Initial Commit

---
+++ JurassicParkApp-iOS17/JurassicParkApp_iOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  JurassicParkApp_iOS17App.swift
+//  JurassicParkApp-iOS17
+//
+//  Created by Nikolay Filipov on 17/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct JurassicParkApp_iOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
